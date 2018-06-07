97f8b37360a86336e4755a48c470101c0cf0d5c5
samuelli97 <samuelli97@gmail.com>
2018-06-07 17:45:28 -0600

    jupyter notebook v1.0

--- SEER_to_csv.py
+++ SEER_to_csv.py
@@ -11,6 +11,7 @@ if len(sys.argv) != 4:
     exit(1)
 
 f_input = open('../SEER_raw/' + sys.argv[2], 'r')
+#f_vars = open('vars.txt', 'r')
 f_output = open('../SEER_parsed_csv/' + sys.argv[3], 'a')
 
 
